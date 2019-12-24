2012b2541f456fd6a1607d468aebb19cd7225bb9
Svitlana Starodub <svitlana_starodub@epam.com>
2019-12-24 16:52:10 +0200

    negative test

--- Client/DropBoxClient.cs
+++ Client/DropBoxClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web.UI.WebControls;
 using DropBoxAPI.DataModels;
 using Newtonsoft.Json;
@@ -10,7 +11,7 @@ namespace DropBoxAPI.Client
 {
     public class DropBoxClient
     {
-        private const string AccessToken = "DBtqFGCsEzAAAAAAAAACt0iGAmdRrLXWSzKyJ9ilqaxpgI1-uwxjio_t60HOIToV";
+        private const string AccessToken = "DBtqFGCsEzAAAAAAAAACuZh4ZWdt1tp8C-rUooxmj3x208JFJbz_Eh4GQSIQfe7A";
         private readonly RestClient _restClient;
         
         public DropBoxClient()
@@ -25,11 +26,27 @@ namespace DropBoxAPI.Client
             request.AddHeader("Content-Type", "application/json");
             request.AddJsonBody(JsonConvert.SerializeObject(createFolderRequestDto));
             var response = _restClient.Execute<CreateFolderResponseDto>(request);
-
+            
             if(!response.IsSuccessful)
                 throw new Exception($"{response.StatusCode}-{response.ErrorMessage}");
 
             return response.Data;
+        }
+
+        public ErrorResponse CreateFolderNegative(CreateFolderRequestDto createFolderRequestDto)
+        {
+            var request = new RestRequest("https://api.dropboxapi.com/2/files/create_folder_v2", Method.POST);
+            AuthorizationHeaders(request);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(JsonConvert.SerializeObject(createFolderRequestDto));
+            var response = _restClient.Execute<ErrorResponseDto>(request);
+
+            return new ErrorResponse
+            {
+                ErrorResponseDto = response.Data,
+                StatusCode =  response.StatusCode
+            };
+
         }
 
         public void DeleteFolder(DeleteFolderRequestDto deleteFolderRequestDto)
---
+++ DataModels/GetFileNegativeResponseDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DropBoxAPI.DataModels
+{
+    public class ErrorResponse
+    {
+        public ErrorResponseDto ErrorResponseDto { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+
+    public class ErrorResponseDto
+    {
+        [JsonProperty("error_summary")]
+        public string ErrorSummary { get; set; }
+    }
+
+
+}
